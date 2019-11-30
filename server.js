ef90510b10a5d9504f43f1484fd45e862e472e9f
Honoka Watanabe <honokawatanabe@Honokas-MacBook-Air.local>
2019-11-30 16:28:26 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/client/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.get('/', require('./routes').index);
+
+app.listen(3000, () => {
+  console.log('> Ready on http://localhost:3000');
+});
